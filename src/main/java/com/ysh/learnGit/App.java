1a93aab4b47e06bde56e4541e1ec05af7f9aca5f
yushaohua <yushaohua@JOHN-PC.jiuxian.local>
2016-09-09 10:36:23 +0800

    init

---
+++ src/main/java/com/ysh/learnGit/App.java
@@ -0,0 +1,13 @@
+package com.ysh.learnGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
